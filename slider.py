daf3f09e35c4ef2a6f0a4192a7fe6838abf79f69
arihanth <arihanth.srikar@research.iiit.ac.in>
2021-02-18 15:05:26 +0530

    Added comments to slider

--- slider.py
+++ slider.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 from screen import display
 from config import height, width, black, Slider_Width, Shrunk_width, Expanded_Width
@@ -7,8 +6,9 @@ from config import height, width, black, Slider_Width, Shrunk_width, Expanded_Wi
 class Slider:
 
     def __init__(self):
-        self.__swidth = 2*Slider_Width
+        self.__swidth = 2*Slider_Width  # defines the slider width
 
+    # initializes the slider on the screen
     def init_slider(self, play_field):
         self.slider_width = np.array(
             [int(width/2)-self.__swidth, int(width/2)+self.__swidth], dtype=int)
@@ -17,6 +17,7 @@ class Slider:
         for i in range(self.slider_width[0], self.slider_width[1]):
             play_field[self.slider_height][i] = '-'
 
+    # updates the slider on the screen
     def __print_slider(self, val, play_field):
         for i in range(1, width-1):
             play_field[self.slider_height][i] = black
@@ -33,18 +34,22 @@ class Slider:
 
         display(play_field)
 
+    # Press a to move left
+    # Press d to move right
     def move(self, play_field, key_press):
         if key_press == 'd':
             self.__print_slider(1, play_field)
         elif key_press == 'a':
             self.__print_slider(-1, play_field)
 
+    # shrinks the slider in half
     def shrink_slider(self, play_field):
         self.__swidth = 2*Shrunk_width
         self.slider_width[0] += Slider_Width
         self.slider_width[1] -= Slider_Width
         self.init_slider(play_field)
 
+    # expands the slider to double
     def expand_slider(self,  play_field):
         self.__swidth = 2*Expanded_Width
         if self.slider_width[0] - Slider_Width <= 1:
