99d6030bb6f93b84ea292d4d8a5600b734c1a7c0
arihanth <arihanth.srikar@research.iiit.ac.in>
2021-03-19 01:56:39 +0530

    Bricks projectile completed

--- powerup.py
+++ powerup.py
@@ -32,33 +32,6 @@ class Powerup:
         self.__next = [self.start[0]+self.x_vel, self.start[1]+self.y_vel]
         self.__count = 3
 
-    # powerup falls down from the block
-    # checks if you have aqcuired it
-
-    # def simulate_powerup(self, play_field, slider_dimensions):
-
-    #     # will enter only if a powerup exists
-    #     if self.start is not None:
-    #         if self.__current[0] < height-2:
-    #             play_field[self.__current[0]][self.__current[1]] = 'powerup'
-    #             play_field[self.__previous[0]
-    #                        ][self.__previous[1]] = black
-    #             # falls down by one
-    #             self.__previous[0] += 1
-    #             self.__current[0] += 1
-    #         else:
-    #             # collision with slider occurs
-    #             play_field[self.__current[0]
-    #                        ][self.__current[1]] = black
-    #             play_field[self.__previous[0]
-    #                        ][self.__previous[1]] = black
-    #             self.start = None
-    #             if self.__current[1] >= slider_dimensions[0] and self.__current[1] <= slider_dimensions[1]:
-    #                 # randomly picks a powerup
-    #                 return self.__temp[np.random.randint(1, 6)], time()
-
-    #     return self.__array_of_powerups, None
-
     def simulate_powerup(self, play_field, slider_dimensions):
 
         self.__count -= 1
